3ee34a7e77dee22b2970446d27479725ac5720a6
talask <35809884+talask@users.noreply.github.com>
2023-04-16 19:04:46 +0000

    Deploying to gh-pages from @ talask/goit-js-hw-09@d18e813eb2d963e6b7e804b8ebc1f743ef717b71 🚀

---
+++ 01-color-switcher.09fb0ad7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");let n;e.addEventListener("click",(function(){e.disabled=!0,n=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),o.addEventListener("click",(function(){e.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.09fb0ad7.js.map
---
+++ 01-color-switcher.734a0b7a.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("body"),n=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");n.addEventListener("click",(function(){n.disabled=!0,t=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),o.addEventListener("click",(function(){n.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.734a0b7a.js.map
